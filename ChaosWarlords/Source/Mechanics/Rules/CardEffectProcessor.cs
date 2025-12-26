3a601015d346abaa895c5edd080bcbb7ef47011e
Itzo <ja.sam.ico@gmail.com>
2025-12-26 01:39:12 +0100

    Fixed no viable target targeting mode

--- ChaosWarlords/Source/Mechanics/Rules/CardEffectProcessor.cs
+++ ChaosWarlords/Source/Mechanics/Rules/CardEffectProcessor.cs
@@ -23,18 +23,19 @@ namespace ChaosWarlords.Source.Systems
         private void ApplyEffect(CardEffect effect, Card sourceCard, MatchContext context)
         {
             int amount = effect.Amount;
+            Player activePlayer = context.ActivePlayer;
 
             switch (effect.Type)
             {
                 case EffectType.GainResource:
                     if (effect.TargetResource == ResourceType.Power)
-                        context.ActivePlayer.Power += amount;
+                        activePlayer.Power += amount;
                     else if (effect.TargetResource == ResourceType.Influence)
-                        context.ActivePlayer.Influence += amount;
+                        activePlayer.Influence += amount;
                     break;
 
                 case EffectType.DrawCard:
-                    context.ActivePlayer.DrawCards(amount);
+                    activePlayer.DrawCards(amount);
                     break;
 
                 case EffectType.Promote:
@@ -45,31 +46,92 @@ namespace ChaosWarlords.Source.Systems
 
                 case EffectType.MoveUnit:
                     // Movement is handled by ActionSystem targeting, this is just confirmation
-                    GameLogger.Log($"{sourceCard.Name}: Movement effect resolved.", LogChannel.Info);
+                    if (context.MapManager.HasValidMoveSource(activePlayer))
+                    {
+                        context.ActionSystem.StartTargeting(ActionState.TargetingMoveSource, sourceCard);
+                        GameLogger.Log($"{sourceCard.Name}: Select a unit to Move.", LogChannel.Input);
+                    }
+                    else
+                    {
+                        GameLogger.Log($"{sourceCard.Name}: No valid units to move.", LogChannel.Warning);
+                    }
                     break;
 
                 case EffectType.Assassinate:
-                    context.ActionSystem.StartTargeting(ActionState.TargetingAssassinate, sourceCard);
-                    GameLogger.Log($"{sourceCard.Name}: Select a valid target to Assassinate.", LogChannel.Input);
+                    if (context.MapManager.HasValidAssassinationTarget(activePlayer))
+                    {
+                        context.ActionSystem.StartTargeting(ActionState.TargetingAssassinate, sourceCard);
+                        GameLogger.Log($"{sourceCard.Name}: Select a valid target to Assassinate.", LogChannel.Input);
+                    }
+                    else
+                    {
+                        GameLogger.Log($"{sourceCard.Name}: No valid targets to Assassinate.", LogChannel.Warning);
+                    }
                     break;
 
                 case EffectType.Supplant:
-                    context.ActionSystem.StartTargeting(ActionState.TargetingSupplant, sourceCard);
-                    GameLogger.Log($"{sourceCard.Name}: Select a valid target to Supplant.", LogChannel.Input);
+                    bool canAssassinate = context.MapManager.HasValidAssassinationTarget(activePlayer);
+                    bool hasTroops = activePlayer.TroopsInBarracks > 0;
+
+                    if (canAssassinate && hasTroops)
+                    {
+                        context.ActionSystem.StartTargeting(ActionState.TargetingSupplant, sourceCard);
+                        GameLogger.Log($"{sourceCard.Name}: Select a valid target to Supplant.", LogChannel.Input);
+                    }
+                    else
+                    {
+                        if (!hasTroops) GameLogger.Log($"{sourceCard.Name}: Cannot Supplant (No Troops in Barracks).", LogChannel.Warning);
+                        else GameLogger.Log($"{sourceCard.Name}: No valid targets to Supplant.", LogChannel.Warning);
+                    }
                     break;
 
                 case EffectType.PlaceSpy:
-                    context.ActionSystem.StartTargeting(ActionState.TargetingPlaceSpy, sourceCard);
-                    GameLogger.Log($"{sourceCard.Name}: Select a Site to Place Spy.", LogChannel.Input);
+                    if (context.MapManager.HasValidPlaceSpyTarget(activePlayer) && activePlayer.SpiesInBarracks > 0)
+                    {
+                        context.ActionSystem.StartTargeting(ActionState.TargetingPlaceSpy, sourceCard);
+                        GameLogger.Log($"{sourceCard.Name}: Select a Site to Place Spy.", LogChannel.Input);
+                    }
+                    else
+                    {
+                         if (activePlayer.SpiesInBarracks <= 0) GameLogger.Log($"{sourceCard.Name}: Cannot Place Spy (No Spies in Barracks).", LogChannel.Warning);
+                         else GameLogger.Log($"{sourceCard.Name}: No valid sites to Place Spy.", LogChannel.Warning);
+                    }
                     break;
 
                 case EffectType.ReturnUnit:
-                    context.ActionSystem.StartTargeting(ActionState.TargetingReturn, sourceCard);
-                    GameLogger.Log($"{sourceCard.Name}: Select a unit to Return.", LogChannel.Input);
+                    // Check for both Spy Return or Troop Return targets?
+                    // The effect "ReturnUnit" usually implies returning an enemy troop OR spy.
+                    // Tyrants rules: "Return a troop" or "Return a spy" are often specific. 
+                    // But if the card just says "Return", it usually means "Return a troop".
+                    // Let's check CardEffects.cs enum if we have separate types?
+                    // It seems we only have ReturnUnit. 
+                    // Looking at ActionSystem.cs: HandleReturn handles TROOPS logic.
+                    // HandleReturnSpy is separate.
+                    // So return implies TROOPS here.
+                    
+                    if (context.MapManager.HasValidReturnTroopTarget(activePlayer))
+                    {
+                        context.ActionSystem.StartTargeting(ActionState.TargetingReturn, sourceCard);
+                        GameLogger.Log($"{sourceCard.Name}: Select a unit to Return.", LogChannel.Input);
+                    }
+                    else
+                    {
+                         GameLogger.Log($"{sourceCard.Name}: No valid units to Return.", LogChannel.Warning);
+                    }
                     break;
 
                 case EffectType.Devour:
-                    context.ActionSystem.TryStartDevourHand(sourceCard);
+                    // context.ActionSystem.TryStartDevourHand(sourceCard) already checks hand count.
+                    // But we can add a log here if we want consistency, or just let it handle it.
+                    // Let's rely on the method but we can check here to be explicit.
+                    if (activePlayer.Hand.Count > 0)
+                    {
+                        context.ActionSystem.TryStartDevourHand(sourceCard);
+                    }
+                    else
+                    {
+                         GameLogger.Log($"{sourceCard.Name}: Hand empty, cannot Devour.", LogChannel.Warning);
+                    }
                     break;
             }
         }
